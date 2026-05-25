df67d9ef614b6523fa05ced6b106281dd3e2a041
rafaelp-dev <dev.rafaelp@gmail.com>
2026-05-25 16:02:40 -0300

    first commit

---
+++ src/main/java/com/savpoint/savpoint/SavpointApplication.java
@@ -0,0 +1,13 @@
+package com.savpoint.savpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavpointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SavpointApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/savpoint/savpoint/SavpointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.savpoint.savpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SavpointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
