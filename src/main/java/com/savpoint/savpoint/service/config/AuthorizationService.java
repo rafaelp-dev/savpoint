6809a1df9f4e345f98daee9ff1599c489017ab28
rafaelp-dev <dev.rafaelp@gmail.com>
2026-06-11 20:47:44 -0300

    feat: add AuthorizationService to load user data

---
+++ src/main/java/com/savpoint/savpoint/service/config/AuthorizationService.java
@@ -0,0 +1,23 @@
+package com.savpoint.savpoint.service.config;
+
+import com.savpoint.savpoint.repositories.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthorizationService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public AuthorizationService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado com email: " + email));
+    }
+}
