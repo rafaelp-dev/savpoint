7ba4f59950d51e9e669ca06a50d861a4da44af78
rafaelp-dev <dev.rafaelp@gmail.com>
2026-06-11 19:52:56 -0300

    feat: add user entity

--- src/main/java/com/savpoint/savpoint/entities/UserEntity.java
+++ src/main/java/com/savpoint/savpoint/entities/UserEntity.java
@@ -1,4 +1,39 @@
 package com.savpoint.savpoint.entities;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
 public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    public UserEntity() {}
+
+    public UserEntity(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.createdAt = LocalDateTime.now();
+    }
 }
