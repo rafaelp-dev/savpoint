9623e0ca2c338f6b1e2c774956d17fd9ac26382d
rafaelp-dev <dev.rafaelp@gmail.com>
2026-06-18 19:01:23 -0300

    feat: add user register request and response

--- src/main/java/com/savpoint/savpoint/dtos/requests/UserRegisterRequest.java
+++ src/main/java/com/savpoint/savpoint/dtos/requests/UserRegisterRequest.java
@@ -2,11 +2,14 @@ package com.savpoint.savpoint.dtos.requests;
 
 import jakarta.validation.constraints.NotBlank;
 
-public record UserLoginRequest(
+public record UserRegisterRequest(
+        @NotBlank (message = "O nome do usuário não pode estar vazio.")
+        String username,
+
         @NotBlank (message = "O email do usuário não pode estar vazio.")
         String email,
 
-        @NotBlank (message = "A senha do usuário não pode estar vazia")
+        @NotBlank (message = "A senha do usuário não pode estar vazia.")
         String password
 ) {
 }
