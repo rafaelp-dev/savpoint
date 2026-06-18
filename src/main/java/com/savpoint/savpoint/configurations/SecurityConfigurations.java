73f8f351cf99ef8b185fb9bd9fc5f19c0d15271d
rafaelp-dev <dev.rafaelp@gmail.com>
2026-06-18 19:46:01 -0300

    feat: add AuthenticationManager bean for authentication configuration

--- src/main/java/com/savpoint/savpoint/configurations/SecurityConfigurations.java
+++ src/main/java/com/savpoint/savpoint/configurations/SecurityConfigurations.java
@@ -2,6 +2,8 @@ package com.savpoint.savpoint.configurations;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -19,5 +21,10 @@ public class SecurityConfigurations {
                 .authorizeHttpRequests(authorizeRequests -> authorizeRequests
                         .anyRequest().authenticated())
                 .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager (AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
