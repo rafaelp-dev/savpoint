4b63b72521aa5a02fe350ea6f6cac01227800351
rafaelp-dev <dev.rafaelp@gmail.com>
2026-06-26 17:02:53 -0300

    feat: add UserProfileRepository and UserProfileService for user profile management

---
+++ src/main/java/com/savpoint/savpoint/repositories/UserProfileRepository.java
@@ -0,0 +1,9 @@
+package com.savpoint.savpoint.repositories;
+
+import com.savpoint.savpoint.entities.UserProfileEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserProfileRepository extends JpaRepository<UserProfileEntity, Long> {
+}
---
+++ src/main/java/com/savpoint/savpoint/service/domain/UserProfileService.java
@@ -0,0 +1,8 @@
+package com.savpoint.savpoint.service.domain;
+
+import com.savpoint.savpoint.repositories.UserProfileRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserProfileService {
+}
