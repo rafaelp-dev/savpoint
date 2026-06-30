bfef7f55b766a4e509c346c36d90ee40a4a9ac80
rafaelp-dev <dev.rafaelp@gmail.com>
2026-06-29 21:11:23 -0300

    feat: add RawgClient configuration for API integration

---
+++ src/main/java/com/savpoint/savpoint/configurations/RawgClient.java
@@ -0,0 +1,17 @@
+package com.savpoint.savpoint.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+
+@Configuration
+public class RawgClient {
+
+    @Bean
+    public RestClient rawgClient() {
+        return RestClient.builder()
+                .baseUrl("https://api.rawg.io/api")
+                .defaultHeader("Content-Type", "application/json")
+                .build();
+    }
+}
