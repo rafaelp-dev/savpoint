a96e7bb79d7e5360b1784fbdd3d9d9fbac9edcfa
rafaelp-dev <dev.rafaelp@gmail.com>
2026-07-04 16:11:40 -0300

    feat: add rawg service for games search

--- src/main/java/com/savpoint/savpoint/service/rawg/RawgService.java
+++ src/main/java/com/savpoint/savpoint/service/rawg/RawgService.java
@@ -1,7 +1,31 @@
 package com.savpoint.savpoint.service.rawg;
 
+import com.savpoint.savpoint.configurations.RawgClient;
+import com.savpoint.savpoint.dtos.rawg.GameDetailsResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RawgService {
+
+    @Value("${rawg.api.key}")
+    private String rawgApiKey;
+
+    private final RawgClient rawgClient;
+
+    public RawgService(RawgClient rawgClient) {
+        this.rawgClient = rawgClient;
+    }
+
+    public GameDetailsResponse getGame (String game) {
+        return rawgClient.restClient().get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/games")
+                        .queryParam("search", game)
+                        .queryParam("key", rawgApiKey)
+                        .queryParam("page_size", 1)
+                        .build())
+                .retrieve()
+                .body(GameDetailsResponse.class);
+    }
 }
